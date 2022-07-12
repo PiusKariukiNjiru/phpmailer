8f5a3c5021736098f4c5eb80b88519c86b04fb96
Pius Kariuki <78341979+PiusKariukiNjiru@users.noreply.github.com>
2022-07-12 23:41:38 +0300

    Add files via upload

--- sendemail.php
+++ sendemail.php
@@ -1,9 +1,9 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 
-require_once 'phpmailer/Exception.php';
-require_once 'phpmailer/PHPMailer.php';
-require_once 'phpmailer/SMTP.php';
+require_once 'Exception.php';
+require_once 'PHPMailer.php';
+require_once 'SMTP.php';
 
 $mail = new PHPMailer(true);
 
@@ -18,13 +18,13 @@ if(isset($_POST['submit'])){
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'youremail@gmail.com'; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'youremailpassword'; // Gmail address Password
+    $mail->Username = 'engkarishpius10@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'piuspius'; // Gmail address Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = '587';
 
-    $mail->setFrom('youremail@gmail.com'); // Gmail address which you used as SMTP server
-    $mail->addAddress('youranyemail@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+    $mail->setFrom('engkarishpius10@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('engkarishpius10@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
     $mail->isHTML(true);
     $mail->Subject = 'Message Received (Contact Page)';
